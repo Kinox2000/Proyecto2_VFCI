e6eceac6beba9bf8e59f19f0ef5788c1d05c1eb4
Kinox2000 <estebancastro@estudiantec.cr>
2022-10-22 18:04:29 -0600

    Create sequence.sv

---
+++ sequence.sv
@@ -0,0 +1,19 @@
+class item_sequence extends uvm_sequence;
+	`uvm_object_utils(item_sequence);
+	function new(string name = "item_sequence");
+		super.new(name);
+	endfunction
+
+	rand int num_items;
+
+	constraint num_items_cons {num_items >= 10; num_items <= 500}
+
+	virtual task body();
+		for(int i = 0; i < num_items; i++)begin
+			multiplication_item mul_item = multiplication_item::type_id::create("mul_item");
+			start_item(mul_item);
+			mul_item.randomize;
+			finis_item(mul_item);
+		end
+        endtask;
+endclass
