67502b6aeb988e43936c729b192c68f7f596c0fa
Kinox2000 <estebancastro@estudiantec.cr>
2022-10-26 00:08:02 -0600

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,10 +1,24 @@
+//`timescale 1ns/ps
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+`include "multiplicador_32_bits_FP_IEEE.sv.txt"
+`include "interface.sv"
+`include "sequence_item.sv"
+`include "sequence.sv"
+`include "monitor.sv"
+`include "driver.sv"
+`include "scoreboard.sv"
+`include "agent.sv"
+`include "environment.sv"
+`include "test.sv"
+
 module tb;
 	reg clk;
 
 	always #10 clk = ~clk;
 	mul_if _if(clk);
-	 multiplicador_32_bits_FP_IEEE u0 (.clk(clk), 
-		 .r_mode(_if.r_mode), 
+			  top u0 (.clk(clk 
+.r_mode(_i),f.r_mode), 
 		 .fp_X(_if.fp_X), 
 		 .fp_Y(_if.fp_Y), 
 		 .fp_Z(_if.fp_Z), 
@@ -12,7 +26,7 @@ module tb;
 		 .udrf(_if.udrf));
 	 initial begin
 		 clk <= 0;
-		 uvm_config_db#(virtual mul_if)::set(null, "uvm_test_top", "des_vif", _if);
+		 uvm_config_db#(virtual mul_if)::set(null, "uvm_test_top", "mul_if", _if);
 		 run_test("base_test");
 	 end
 
