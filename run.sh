7475cb18f582a754a6dc080cd95b63ae42c63dbf
Kinox2000 <estebancastro@estudiantec.cr>
2022-10-26 13:36:56 -0600

    run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+source /mnt/vol_NFS_rh003/estudiantes/archivos_config/synopsys_tools.sh;
+rm -rfv `ls |grep -v ".*\.sv\|.*\.sh"`;
+
+vcs -Mupdate testbench.sv -o salida -full64 -debug_acc+all -debug_region+cell+encrypt -sverilog -l log_test -ntb_opts uvm +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert;
+
+./salida +UVM_VERBOSITY=UVM_HIGH +UVM_TESTNAME=test1 +ntb_random_seed =1
