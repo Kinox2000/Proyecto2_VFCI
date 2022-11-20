929f27d52bb3caa1ad3a9a0e91e263fdc540113f
Kinox2000 <estebancastro@estudiantec.cr>
2022-11-19 21:20:01 -0600

    Se comenta el código

--- monitor.sv
+++ monitor.sv
@@ -1,11 +1,11 @@
-class monitor extends uvm_monitor;
+class monitor extends uvm_monitor;//Se crea una clase monitor para revisar las salidas del DUT y enviarlas al scoreboard
 	`uvm_component_utils(monitor)
 	function new(string name = "monitor", uvm_component parent = null);
 		super.new(name, parent);
 	endfunction
 
-	uvm_analysis_port #(multiplication_item) mon_analysis_port;
-	virtual mul_if vif;
+	uvm_analysis_port #(multiplication_item) mon_analysis_port;//Puerto para enviar datos al scoreboard
+	virtual mul_if vif;//Interfaz para comunicarse con el DUT y obtener sus entradas y salidas
 
 	virtual function void build_phase(uvm_phase phase);
 	  super.build_phase(phase);
@@ -19,8 +19,8 @@ class monitor extends uvm_monitor;
 	  super.run_phase(phase);
 
       forever @(vif.cb) begin
-        multiplication_item mul_item = multiplication_item::type_id::create("mul_item");
-		mul_item.r_mode = vif.r_mode;
+        multiplication_item mul_item = multiplication_item::type_id::create("mul_item");// Se crea un objeto y se guardan los valores de entrada y salida del DUT para enviarlos al scoreboard
+	    mul_item.r_mode = vif.r_mode;
 	    mul_item.fp_X = vif.fp_X;
 	    mul_item.fp_Y = vif.fp_Y;
 	    mul_item.fp_Z = vif.cb.fp_Z;
@@ -28,8 +28,7 @@ class monitor extends uvm_monitor;
 	    mul_item.udrf = vif.cb.udrf;
 	    mul_item.mul_time = $time; 
 	    mon_analysis_port.write(mul_item);
-        `uvm_info("Monitor: ", $sformatf("r_mode fp_X fp_Y fp_Z:%h %h %h %h", mul_item.r_mode, mul_item.fp_X, mul_item.fp_Y, mul_item.fp_Z), UVM_LOW);
-		 //`uvm_info("Monitor: ", $sfotmatf("Item: ", mul_item.do_print()), UVM_MEDIUM)
+        `uvm_info("Monitor: ", $sformatf("r_mode fp_X fp_Y fp_Z:%h %h %h %h", mul_item.r_mode, mul_item.fp_X, mul_item.fp_Y, mul_item.fp_Z), UVM_MEDIUM);
 	  end
   	endtask
 
