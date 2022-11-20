ae364bd969d2aaf39ab97de4a4397ef5a2ecdbad
Kinox2000 <estebancastro@estudiantec.cr>
2022-11-19 22:26:18 -0600

    Se comenta el código

--- sequence_item.sv
+++ sequence_item.sv
@@ -16,17 +16,17 @@ class multiplication_item extends uvm_sequence_item;
 		super.new(name);
 	endfunction
 
-	constraint r {r_mode >= 0; r_mode <= 3;}
-	constraint X {fp_X[22:0] >= 0; fp_X[22:0] < 8388608;}
-	constraint X_e{fp_X[30:23] >= 0; fp_X[30:23] < 256;}
-	constraint X_s{fp_X[31] >= 0; fp_X[31] <= 1;}
-	constraint Y {fp_Y[22:0] >= 0; fp_Y[22:0] < 8388608;}
-	constraint Y_e{fp_Y[30:23] >= 0; fp_Y[30:23] < 256;}
-	constraint Y_s{fp_Y[31] >= 0; fp_Y[31] <= 1;}
-	constraint d {delay>=0; delay <= 20;}
-    	constraint underflow { (fp_X[30:23] + fp_Y[30:23] <=127) | fp_X[30:0]==0 | fp_Y[30:0]==0 ; }	
-    	constraint overflow {fp_X[30:23] + fp_Y[30:23] >=382 ;}
-    	constraint NaN {(fp_X[30:23]==8'b1111_1111) | (fp_Y[30:23]==8'b1111_1111) ;}
+	constraint r {r_mode >= 0; r_mode <= 3;}//Constraint para randomizar el modo de redondeo 
+	constraint X {fp_X[22:0] >= 0; fp_X[22:0] < 8388608;}//Constraint para randomizar los bits 22 a 0 para el test random
+	constraint X_e{fp_X[30:23] >= 0; fp_X[30:23] < 256;}//Constraint para randomizar los bits del exponente para el test random
+	constraint X_s{fp_X[31] >= 0; fp_X[31] <= 1;}//Constraint para randomizar el signo de la entrada
+	constraint Y {fp_Y[22:0] >= 0; fp_Y[22:0] < 8388608;}//Constraint para randomizar los bits 22 a 0 para el test random
+	constraint Y_e{fp_Y[30:23] >= 0; fp_Y[30:23] < 256;}//Constraint para randomizar los bits del exponente para el test random
+	constraint Y_s{fp_Y[31] >= 0; fp_Y[31] <= 1;}//Constraint para randomizar el signo de la entrada
+	constraint d {delay>=0; delay <= 20;}//Constraint para randomizar el delay
+    	constraint underflow { (fp_X[30:23] + fp_Y[30:23] <=127) | fp_X[30:0]==0 | fp_Y[30:0]==0 ; }//Constraint para randomizar las entrada para el test de underflow
+    	constraint overflow {fp_X[30:23] + fp_Y[30:23] >=382 ;}//Constraint para randomizar las entrada para el test de overflow
+    	constraint NaN {(fp_X[30:23]==8'b1111_1111) | (fp_Y[30:23]==8'b1111_1111) ;}//Constraint para randomizar las entrada para el test de NaN
 
 	virtual function string print();
 		return $sformatf("Redondeo: %0b, X: %0h, Y: %0h", r_mode, fp_X, fp_Y);
